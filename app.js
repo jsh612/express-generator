3b5abdfa5dc7772a66446616cdcebb021ec6e6e2
seungho Jeon <urijsh612@gmail.com>
2019-09-21 20:22:56 +0900

    session part1

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+console.log("스토어보기",FileStore)
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -30,18 +33,32 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+
+/* cookie-parser는 더이상 필요하지 않으므로 주석처리한다.
+그 이유는 express-session은 쿠키를 바로 읽거나 쓸 수 있기때문이다. */
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 //미들웨어는 순서데로 실행된다.
 //app.use(express.static(path.join(__dirname, 'public'))); 이 미들웨어 전에 해주어야
 //유저가 서버에 접근하기전에 권한 유무를 확인하여 통제 가는하다.
 function auth(req, res, next) {
 
-  //req.signedCookies 는 요청한는 쿠키들을 보여준다.
-  console.log("req.signedCookies", req.signedCookies);
+  /* //req.signedCookies 는 요청한는 쿠키들을 보여준다.
+  console.log("req.signedCookies", req.signedCookies); */
 
-  if (!req.signedCookies.user) {
-    //만약 요청하는 쿠키들 중에 user가 없다면.
+  //epress-session 모듈은 request 메시지에 req.session을 추가시킨다.
+  console.log("req.session 내용보기",req.session)
+
+  if (!req.session.user) {
+    //만약 요청하는 쿠키(세션이용시 세션)들 중에 user가 없다면.
     //이것은 아직 해당 유저가 권한(like 로그인)을 얻지 못했음을 의미.
     let authHeader = req.headers.authorization;
   
@@ -66,8 +83,11 @@ function auth(req, res, next) {
     var password = auth[1];
   
     if (usertName === 'admin' && password === 'password') {
-      //클라이언트가 쿠키를 설정
-      res.cookie('user', 'admin', {signed: true})
+      /* //클라이언트가 쿠키를 설정
+      res.cookie('user', 'admin', {signed: true}) */
+
+      //epress-session 사용시에는 cookie를 작성하는대신 다음과 같이 session을 이용한다
+      req.session.user = 'admin';
 
       //네임과 비번이 일치 한다면 
       // 다음 미들웨어 실행가능 (즉, 권한이 입증되어서 서버와 통신 가능하게된것이다.)
@@ -82,7 +102,7 @@ function auth(req, res, next) {
   else {
     //클라이언트가 이미 쿠키를 생성했다면
     //에지 해당 서버에 접근해서 아래에 이어지는 코드 진행
-    if (req.signedCookies.user === 'admin') {
+    if (req.session.user === 'admin') {
       next()
     } else {
       //에초에 쿠키 생성시 정확인 권한인증이 된 경우에만 쿠키를 생성하였으므로
